31168f5d2b7cac77907996f9f652ae6801bb48ab
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2021-11-01 10:55:33 -0500

    setting up files

---
+++ fakeBusiness.js
@@ -0,0 +1,32 @@
+export let Businesses = [
+  {
+    name: "business1",
+    description: "does stuff",
+    address: "12345 street",
+    hours: "10am-2pm",
+  },
+  {
+    name: "business2",
+    description: "doesnt do stuff",
+    address: "1235 blvd",
+    hours: "11am-2pm",
+  },
+  {
+    name: "business3",
+    description: "does more stuff",
+    address: "5678 street",
+    hours: "7am-2pm",
+  },
+  {
+    name: "business4",
+    description: "does nothing",
+    address: "123 rd",
+    hours: "6am-11pm",
+  },
+  {
+    name: "business5",
+    description: "stuff",
+    address: "2345 street",
+    hours: "11am-12pm",
+  },
+];
